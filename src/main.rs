81e9879263469ac1bb1d63dd4da1881094a3a202
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-06-19 12:51:34 +0200

    Basic CLI Setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod config;
+
+use self::config::Config;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Arguments {
+    #[clap(short, long, env = "ARAKCONFIG", default_value = "arak.toml")]
+    config: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+    let config = Config::load(&args.config).expect("failed to load configuration");
+
+    println!("{config:#?}");
 }

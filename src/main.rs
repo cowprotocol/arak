fbc392a4c87008a0bfb4c00757fdbd6c993b8e94
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-06-20 15:43:21 +0200

    Fetch Blocks (#12)
    This PR adds a block fetching loop to `main`. Ideally, I would already
    start piping events to the decoders in order to produce decoded event
    fields, but I ran into the issue that `eth_getBlockBy*` does not
    actually return logs.
    
    Decided to use my own `ethrpc` crate. Its just a light-weight library
    containing type definitions and `Method` implementations that match RPC
    method parameters to response types.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ mod decoder;
 mod sqlite;
 
 use self::{config::Config, decoder::Decoder};
+use anyhow::{Context, Result};
 use clap::Parser;
+use ethrpc::{
+    eth,
+    types::{BlockTag, Digest, Hydrated},
+};
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -13,20 +18,59 @@ struct Arguments {
     config: PathBuf,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let args = Arguments::parse();
-    let config = Config::load(&args.config).expect("failed to load configuration");
+    let config = Config::load(&args.config).context("failed to load configuration")?;
 
     tracing::info!("{config:#?}");
 
+    let eth = ethrpc::http::Client::new(config.ethrpc);
     let decoders = config
         .events
-        .iter()
-        .map(|event| Decoder::new(&event.signature).expect("unsupported event signature"))
-        .collect::<Vec<_>>();
-    if let Some(decoder) = decoders.first() {
-        tracing::debug!("{:?}", decoder.decode(&Default::default()));
+        .into_iter()
+        .map(|event| {
+            Ok((
+                event.name,
+                Decoder::new(&event.signature).context("unsupported event signature")?,
+            ))
+        })
+        .collect::<Result<Vec<_>>>()?;
+
+    let mut last_block = eth
+        .execute_empty(eth::BlockNumber)
+        .await
+        .context("failed to get starting block number")?
+        - 1;
+    loop {
+        let block = eth
+            .execute(
+                eth::GetBlockByNumber,
+                (BlockTag::Latest.into(), Hydrated::Yes),
+            )
+            .await?
+            .context("missing latest block data")?;
+
+        if last_block >= block.number {
+            continue;
+        }
+        tracing::info!(?block.number, "new block");
+        last_block = block.number;
+
+        let logs = get_block_logs(block.hash);
+        for log in &logs {
+            for (name, decoder) in &decoders {
+                if let Ok(fields) = decoder.decode(log) {
+                    tracing::info!(?name, ?fields, "event");
+                }
+            }
+        }
     }
+}
+
+fn get_block_logs(_: Digest) -> Vec<solabi::log::Log<'static>> {
+    // todo
+    vec![]
 }

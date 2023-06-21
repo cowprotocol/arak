d14a1f003e0884691bade3db36ccb30c69b12823
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-06-21 21:19:31 +0200

    Update Database Trait (#14)
    ☝️
    
    Updates the database trait to index events per block (instead of per
    event) and adds a new function for fetching the last indexed block for a
    particular event.

--- src/database.rs
+++ src/database.rs
@@ -3,8 +3,30 @@
 // - Implement this for Postgres in addition to Sqlite. Postgres will likely have a native async backend, so we should have the trait be async and internally `spawn_blocking` for use of non async rusqlite.
 // - Think about whether the trait should be Send + Sync and whether methods should take mutable Self.
 
-use anyhow::Result;
-use solabi::{abi::EventDescriptor, value::Value};
+use anyhow::{Context, Result};
+use solabi::{abi::EventDescriptor, ethprim::Address, value::Value};
+use std::{
+    collections::{hash_map, HashMap},
+    iter,
+};
+
+/// The last indexed block for a given event.
+#[derive(Debug)]
+pub struct IndexedBlock<'a> {
+    pub event: &'a str,
+    pub number: u64,
+}
+
+/// An emitted event log.
+#[derive(Debug)]
+pub struct Log<'a> {
+    pub event: &'a str,
+    pub block_number: u64,
+    pub log_index: u64,
+    pub transaction_index: u64,
+    pub address: Address,
+    pub fields: Vec<Value>,
+}
 
 /// Abstraction over specific SQL like backends.
 ///
@@ -26,18 +48,59 @@ pub trait Database {
     /// - A table for `name` already exists with an incompatible event signature.
     fn prepare_event(&mut self, name: &str, event: &EventDescriptor) -> Result<()>;
 
-    /// Store an event in the database.
+    /// Retrieves the last indexed block for the specified event.
+    fn event_block(&mut self, name: &str) -> Result<u64>;
+
+    /// Updates the storage in a single transaction. It updates two things:
+    /// - `blocks` specifies updates to the last updated block for events; this
+    ///   will change the value that is read from `event_block`.
+    /// - `logs` specified new logs to append to the database.
     ///
     /// Errors:
     ///
-    /// - `prepare_event` has not been successfully called with this `name`.
-    /// - `fields` do not match the event signature specified in the successful call to `prepare_event` with this `name`.
-    fn store_event(
-        &mut self,
-        name: &str,
-        block_number: u64,
-        log_index: u64,
-        address: &[u8; 20],
-        fields: &[Value],
-    ) -> Result<()>;
+    /// - `prepare_event` has not been successfully called with `event` field
+    ///   from one or more of the specified `blocks` or `logs`.
+    /// - `fields` do not match the event signature specified in the successful
+    ///   call to `prepare_event` with this `event` name for one or more `logs`.
+    fn update(&mut self, blocks: &[IndexedBlock], logs: &[Log]) -> Result<()>;
+}
+
+#[derive(Default)]
+pub struct Dummy {
+    events: HashMap<String, u64>,
+}
+
+impl Database for Dummy {
+    fn prepare_event(&mut self, name: &str, _: &EventDescriptor) -> Result<()> {
+        let hash_map::Entry::Vacant(entry) = self.events.entry(name.to_string()) else {
+            anyhow::bail!("duplicate event {name}");
+        };
+        entry.insert(0);
+        Ok(())
+    }
+
+    fn event_block(&mut self, name: &str) -> Result<u64> {
+        self.events
+            .get(name)
+            .copied()
+            .context("missing event {name}")
+    }
+
+    fn update(&mut self, blocks: &[IndexedBlock], logs: &[Log]) -> Result<()> {
+        let events = iter::empty()
+            .chain(blocks.iter().map(|block| block.event))
+            .chain(logs.iter().map(|log| log.event));
+        for event in events {
+            anyhow::ensure!(self.events.contains_key(event), "missing event {event}");
+        }
+
+        for block in blocks {
+            *self.events.get_mut(block.event).unwrap() = block.number;
+        }
+        for log in logs {
+            tracing::info!(?log, "added log");
+        }
+
+        Ok(())
+    }
 }
